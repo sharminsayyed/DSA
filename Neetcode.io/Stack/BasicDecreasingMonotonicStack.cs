3eed1d45290d9bb78d5599634d28dc2fd99e2f18
Sharmin Sayyed <sharminsayyed029@gmail.com>
2025-09-04 22:50:41 +0530

    added

---
+++ Neetcode.io/Stack/BasicDecreasingMonotonicStack.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] NextGreaterElements(int[] nums) {
+        // here we store the decreasing monotonic stack
+        // time complexity =O(2n) space =O(n)
+        int n = nums.Length;
+        int[] nge = new int[n];
+        var stack = new Stack<int>();
+        for(int i = n-1 ;i >=0 ;i--){
+            while(stack.Count >0 && stack.Peek() < nums[i]){
+                stack.Pop();
+            }
+            if(stack.Count == 0){
+                nge[i] =-1;
+            }
+            else{
+                nge[i] = stack.Peek();
+            }
+            stack.Push(nums[i]);
+
+        }
+        return nge;
+
+    }
+}
